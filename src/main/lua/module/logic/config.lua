96cc109f6679db29d518bf4c947026eb0d7a33ab
Biklom <Biklom@users.noreply.github.com>
2017-06-20 14:30:22 +0200

    saving lua scripts

---
+++ src/main/lua/module/logic/config.lua
@@ -0,0 +1,44 @@
+return {
+    color_by_element = {
+            Fire        = "rgba(255, 0, 0, 0.5)",
+            Wood        = "rgba(0, 255, 0, 1)",
+            Water       = "rgba(40,178, 247,1)",
+            Light       = "rgba(255, 255, 0, 1)",
+            Shadow      = "rgba(183, 0, 255, 1)",
+            Fire_soft   = "rgba(255, 0, 0, 0.2)",
+            Wood_soft   = "rgba(0, 255, 0, 0.5)",
+            Water_soft  = "rgba(40,178, 247,0.5)",
+            Light_soft  = "rgba(255, 255, 0, 0.5)",
+            Shadow_soft = "rgba(183, 0, 255, 0.5)",
+        },
+    flags_by_lang = {
+            en = "Britain.png",
+            fr = "France.png",
+            de = "GERM0001.png",
+            it = "Italy.png",
+            es = "Spain.png",
+            en_16 = "Britain_16.png",
+            fr_16 = "France_16.png",
+            de_16 = "GERM0001_16.png",
+            it_16 = "Italy_16.png",
+            es_16 = "Spain_16.png",
+        },
+    rarity_table = {
+            common          = "C",
+            common_plus     = "C+",
+            uncommon        = "U",
+            uncommon_plus   = "U+",
+            rare            = "R",
+            rare_plus       = "R+",
+            ultrarare       = "UR",
+            ultrarare_plus  = "UR+",
+            legendary       = "L"
+        },
+    box_icon_by_type = {
+            blue1 = "blue_box.png",
+            blue2 = "blue_box.png",
+            red1 = "red_box.png",
+            red2 = "red_box.png",
+            bonus = "Lock_64.png"
+        }
+}
